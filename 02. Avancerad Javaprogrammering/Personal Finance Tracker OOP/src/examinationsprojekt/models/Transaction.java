ed20d1fadbcd9c576a74d0032d11b44243743306
elinasamuelsson <elina.samuelsson@live.com>
2025-10-21 22:03:22 +0200

    Update Transaction.java
    Switching from using UUID to generating a shorter id as the risk of repeating is very low and short ids are easier to read and search.

--- 02. Avancerad Javaprogrammering/Personal Finance Tracker OOP/src/examinationsprojekt/models/Transaction.java
+++ 02. Avancerad Javaprogrammering/Personal Finance Tracker OOP/src/examinationsprojekt/models/Transaction.java
@@ -1,26 +1,36 @@
 package examinationsprojekt.models;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 public class Transaction {
-    private final UUID id;
+    private final String id;
     private final double amount;
     private final LocalDateTime time;
     private final TransactionTypes type;
     private final String description;
 
     public Transaction(double amount, LocalDateTime time, TransactionTypes type, String description) {
-        this.id = UUID.randomUUID();
+        this.id = idGenerator();
         this.amount = amount;
         this.time = time;
         this.type = type;
         this.description = description;
     }
 
-    public UUID getId() { return id; }
+    public String getId() { return id; }
     public double getAmount() { return amount; }
     public LocalDateTime getTime() { return time; }
     public TransactionTypes getType() { return type; }
     public String getDescription() { return description; }
+
+    public String idGenerator() {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder id = new StringBuilder();
+
+        for (int i = 0; i < 8; i++) {
+            id.append(chars.charAt((int) (Math.random() * chars.length())));
+        }
+
+        return id.toString();
+    }
 }
