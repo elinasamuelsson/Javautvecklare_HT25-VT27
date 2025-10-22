59399035063e0474984f3bbdd9cd1b8289577330
elinasamuelsson <elina.samuelsson@live.com>
2025-10-22 09:08:59 +0200

    Update ReadUserTerminalInput.java
    Clarified that input can be made by adding a > under the prompt and next to where input will be made.

--- 02. Avancerad Javaprogrammering/Personal Finance Tracker OOP/src/examinationsprojekt/utils/ReadUserTerminalInput.java
+++ 02. Avancerad Javaprogrammering/Personal Finance Tracker OOP/src/examinationsprojekt/utils/ReadUserTerminalInput.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ReadUserTerminalInput implements IReadUserInput {
     @Override
     public String stringInput() {
+        System.out.println(" >");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
@@ -14,6 +15,7 @@ public class ReadUserTerminalInput implements IReadUserInput {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             try {
+                System.out.println(" >");
                 return Double.parseDouble(scanner.nextLine());
             } catch (NumberFormatException exception) {
                 System.out.println("Please enter a valid number.");
