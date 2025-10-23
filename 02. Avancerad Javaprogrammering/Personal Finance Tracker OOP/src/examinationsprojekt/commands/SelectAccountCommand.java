5602ab9b36b4eea6c41468f2782150843af592a4
elinasamuelsson <elina.samuelsson@live.com>
2025-10-23 15:49:56 +0200

    Update SelectAccountCommand.java
    Added SelectAccountCommand logic

--- 02. Avancerad Javaprogrammering/Personal Finance Tracker OOP/src/examinationsprojekt/commands/SelectAccountCommand.java
+++ 02. Avancerad Javaprogrammering/Personal Finance Tracker OOP/src/examinationsprojekt/commands/SelectAccountCommand.java
@@ -1,7 +1,35 @@
 package examinationsprojekt.commands;
 
-public class SelectAccountCommand implements ICommand{
+import examinationsprojekt.managers.CurrentStateManager;
+import examinationsprojekt.models.Account;
+import examinationsprojekt.repository.IRepository;
+import examinationsprojekt.repository.ListRepository;
+import examinationsprojekt.utils.IReadUserInput;
+import examinationsprojekt.utils.ReadUserTerminalInput;
+
+public class SelectAccountCommand implements ICommand {
+    private final IReadUserInput input = new ReadUserTerminalInput();
+
     public void run() {
-        System.out.println("select account command");
+        IRepository repository = new ListRepository();
+
+        System.out.println("Which of the following accounts do you want to use?");
+        System.out.println();
+
+        for (Account account : repository.read()) {
+            System.out.print((repository.read().indexOf(account) + 1) + ":");
+            System.out.println("\t" + account.getName() + ", " + account.getType().getTypeDescription());
+            System.out.println("\t" + account.getOwner());
+            System.out.println();
+        }
+
+        int userInput = input.intInput();
+
+        for (Account account : repository.read()) {
+            if (userInput == (repository.read().indexOf(account) + 1)) {
+                CurrentStateManager.setCurrentAccount(account);
+                System.out.println(account.getName() + " selected.");
+            }
+        }
     }
 }
