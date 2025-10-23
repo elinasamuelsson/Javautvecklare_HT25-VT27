ab757512c65ddb3e30988e571bf943406e656933
elinasamuelsson <elina.samuelsson@live.com>
2025-10-23 15:58:43 +0200

    Update DeleteAccountCommand.java
    Added logic to DeleteAccountCommand

--- 02. Avancerad Javaprogrammering/Personal Finance Tracker OOP/src/examinationsprojekt/commands/DeleteAccountCommand.java
+++ 02. Avancerad Javaprogrammering/Personal Finance Tracker OOP/src/examinationsprojekt/commands/DeleteAccountCommand.java
@@ -1,7 +1,35 @@
 package examinationsprojekt.commands;
 
+import examinationsprojekt.managers.CurrentStateManager;
+import examinationsprojekt.models.Account;
+import examinationsprojekt.repository.IRepository;
+import examinationsprojekt.repository.ListRepository;
+import examinationsprojekt.utils.IReadUserInput;
+import examinationsprojekt.utils.ReadUserTerminalInput;
+
 public class DeleteAccountCommand implements ICommand {
+    private final IReadUserInput input = new ReadUserTerminalInput();
+
     public void run() {
-        System.out.println("delete account command");
+        IRepository repository = new ListRepository();
+
+        System.out.println("Which of the following accounts do you want to delete?");
+        System.out.println();
+
+        for (Account account : repository.read()) {
+            System.out.print((repository.read().indexOf(account) + 1) + ":");
+            System.out.println("\t" + account.getName() + ", " + account.getType().getTypeDescription());
+            System.out.println("\t" + account.getOwner());
+            System.out.println();
+        }
+
+        int userInput = input.intInput();
+
+        for (Account account : repository.read()) {
+            if (userInput == (repository.read().indexOf(account) + 1)) {
+                repository.delete(account);
+                System.out.println(account.getName() + " deleted.");
+            }
+        }
     }
 }
