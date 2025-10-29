fff5115346e11aa97c27c4cbd0c93b44fd14c9f7
elinasamuelsson <elina.samuelsson@live.com>
2025-10-29 07:59:13 +0100

    Update ViewTransactionsCommand.java
    Added logic for view monthly and added what seemed like unnecessary code.

--- 02. Avancerad Javaprogrammering/Personal Finance Tracker OOP/src/examinationsprojekt/commands/ViewTransactionsCommand.java
+++ 02. Avancerad Javaprogrammering/Personal Finance Tracker OOP/src/examinationsprojekt/commands/ViewTransactionsCommand.java
@@ -9,6 +9,8 @@ import examinationsprojekt.utils.IReadUserInput;
 import examinationsprojekt.utils.ReadUserTerminalInput;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.InputMismatchException;
@@ -34,62 +36,113 @@ public class ViewTransactionsCommand implements ICommand {
         }
 
         try {
-            printTransactions(option, accountToView);
+            List<Transaction> transactions = sortAllTransactions(accountToView);
+
+            if (option.equals(ViewOptions.YEARLY)) {
+                viewYearly(transactions);
+            } else if (option.equals(ViewOptions.MONTHLY)) {
+                viewMonthly(transactions);
+            } else if (option.equals(ViewOptions.WEEKLY)) {
+                System.out.println("printed weekly transactions list");
+            } else if (option.equals(ViewOptions.DAILY)) {
+                System.out.println("printed daily transactions list");
+            } else if (option.equals(ViewOptions.CATEGORY)) {
+                System.out.println("printed transactions list by category");
+            }
         } catch (NullPointerException exception) {
             System.out.println("No account to print.");
         }
     }
 
-    private void printTransactions(ViewOptions option, Account account) {
-        List<Transaction> transactions = sortAllTransactions(account);
+    private void viewYearly(List<Transaction> transactions) {
+        int dateToPrint = LocalDateTime.now().getYear();
 
-        if (option.equals(ViewOptions.YEARLY)) {
-            int dateToPrint = LocalDateTime.now().getYear();
-
-            while (true) {
-                System.out.println("________________" + dateToPrint + "________________\n");
-                for (Transaction transaction : transactions) {
-                    if (transaction.getTime().getYear() == dateToPrint) {
-                        printTransaction(transaction);
-                    }
-                }
-
-                System.out.println("What do you want to do?");
-                System.out.println("1. View previous year");
-                System.out.println("2. View next year");
-                System.out.println("3. Manually select year");
-                System.out.println("0. Return");
-
-                String userInput = input.stringInput();
-                switch (userInput) {
-                    case "1":
-                        dateToPrint--;
-                        break;
-                    case "2":
-                        dateToPrint++;
-                        break;
-                    case "3":
-                        try {
-                            System.out.println("Enter the year you wish to view: ");
-                            dateToPrint = input.intInput();
-                        } catch (InputMismatchException exception) {
-                            System.out.println("Please enter a valid year.");
-                        }
-                        break;
-                    case "0":
-                        return;
-                    default:
-                        System.out.println("This option does not exist. Try again.");
+        while (true) {
+            System.out.println("----------------" + dateToPrint + "----------------");
+            System.out.println();
+            for (Transaction transaction : transactions) {
+                if (transaction.getTime().getYear() == dateToPrint) {
+                    printTransaction(transaction);
                 }
             }
-        } else if (option.equals(ViewOptions.MONTHLY)) {
-            System.out.println("printed monthly transactions list");
-        } else if (option.equals(ViewOptions.WEEKLY)) {
-            System.out.println("printed weekly transactions list");
-        } else if (option.equals(ViewOptions.DAILY)) {
-            System.out.println("printed daily transactions list");
-        } else if (option.equals(ViewOptions.CATEGORY)) {
-            System.out.println("printed transactions list by category");
+
+            System.out.println("What do you want to do?");
+            System.out.println("1. View previous year");
+            System.out.println("2. View next year");
+            System.out.println("3. Manually select year");
+            System.out.println("0. Return");
+
+            String userInput = input.stringInput();
+            switch (userInput) {
+                case "1":
+                    dateToPrint--;
+                    break;
+                case "2":
+                    dateToPrint++;
+                    break;
+                case "3":
+                    try {
+                        System.out.println("Enter the year you wish to view: ");
+                        dateToPrint = input.intInput();
+                    } catch (InputMismatchException exception) {
+                        System.out.println("Please enter a valid year.");
+                    }
+                    break;
+                case "0":
+                    return;
+                default:
+                    System.out.println("This option does not exist. Try again.");
+            }
+        }
+    }
+
+    private void viewMonthly(List<Transaction> transactions) {
+        LocalDateTime dateToPrint = LocalDateTime.now();
+
+        while (true) {
+            System.out.println("----------------" + dateToPrint.getMonth().toString() + " " +
+                    dateToPrint.getYear() + "----------------");
+            System.out.println();
+            for (Transaction transaction : transactions) {
+                if (transaction.getTime().getMonthValue() == dateToPrint.getMonthValue()
+                        && transaction.getTime().getYear() == dateToPrint.getYear()) {
+                    printTransaction(transaction);
+                }
+            }
+
+            System.out.println("What do you wish to do?");
+            System.out.println("1. View previous month");
+            System.out.println("2. View next month");
+            System.out.println("3. Manually select month");
+            System.out.println("0. Return");
+
+            String userInput = input.stringInput();
+            switch (userInput) {
+                case "1":
+                    dateToPrint = dateToPrint.minusMonths(1);
+                    break;
+                case "2":
+                    dateToPrint = dateToPrint.plusMonths(1);
+                    break;
+                case "3":
+                    try {
+                        System.out.println("Enter date of the day you wish to view: ");
+                        System.out.println("Please use the format YYYY-MM");
+                        String dateInput = input.stringInput();
+
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+                        dateToPrint = LocalDateTime.parse((dateInput + "-01 12:00"), formatter);
+                    } catch (DateTimeParseException exception) {
+                        System.out.println("Please enter a valid date.");
+                        System.out.println("Please use the format YYYY-MM");
+
+                    }
+                    break;
+                case "0":
+                    return;
+                default:
+                    System.out.println("This option does not exist. Try again.");
+            }
         }
     }
 
